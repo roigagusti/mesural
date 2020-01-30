2388144e2de74c7f40d0a5d17215714974f5f775
roigagusti <gutiroma@hotmail.com>
2020-01-30 18:40:56 +0100

    all

--- sandbox/index.php
+++ sandbox/index.php
@@ -73,142 +73,22 @@ include_once("sections/visitors.php");
 
 			<section>
 				<div class="container">
-					<div class="row mb64 mb-xs-24">
-						<div class="col-sm-12 text-center">
-							<h2><?php echo $text['Our system']; ?></h2>
-						</div>
-					</div>	
-					<div class="row">
-						<div class="col-md-3 text-center">
-							<div class="service-icon">
-								<i class="fas fa-microchip"></i>
-							</div>
-							<h3><?php echo $text['Titol 1']; ?></h3>
-							<p><?php echo $text['Descripcio 1']; ?></p>
-						</div>
-						<div class="col-md-3 text-center">
-							<div class="service-icon">
-								<i class="fas fa-server"></i>
-							</div>
-							<h3><?php echo $text['Titol 2']; ?></h3>
-							<p><?php echo $text['Descripcio 2']; ?></p>
-						</div>
-						<div class="col-md-3 text-center">
-							<div class="service-icon">
-								<i class="fas fa-code"></i>
-							</div>
-							<h3><?php echo $text['Titol 3']; ?></h3>
-							<p><?php echo $text['Descripcio 3']; ?></p>
-						</div>
-						<div class="col-md-3 text-center">
-							<div class="service-icon">
-								<i class="far fa-check-circle"></i>
-							</div>
-							<h3><?php echo $text['Titol 4']; ?></h3>
-							<p><?php echo $text['Descripcio 4']; ?></p>
-						</div>
-					</div>
+					<h2>Mesural Capsule</h2>
+					<h3>The only thing that change is everything</h3>
+					<a href="#">More information</a>
+					<a href="#">Buy</a>
+					<img src="img/capsule.png" />
+					<p>Te resultará totalmente familiar y totalmente revolucionario. Crea las mejores soluciones constructivas con la tranquilidad en todo momento. Presentamos la posibilidad de innovar bajo control.</p>
 				</div>
 			</section>
 
-			<section class="bg-secondary" id="services">
+			<section class="bg-secondary" >
 				<div class="container">
-					<div class="row mb64 mb-xs-24">
-						<div class="col-sm-12 text-center">
-							<h2><?php echo $text['Services']; ?></h2>
-						</div>
-					</div>
-					<div class="row etapa">
-						<div class="col-md-6">
-							<img src="img/serveis/mac+dashboard.png" alt="Previsualización en Macbook">
-						</div>
-						<div class="col-md-6 descripcio">
-							<h3><?php echo $text['Etapa. Titol 1']; ?></h3>
-							<p><?php echo $text['Etapa. Descripcio 1']; ?></p>
-						</div>
-					</div>
-
-					<div class="row etapa">
-						<div class="col-md-6 mobile">
-							<img src="img/serveis/ipad+iphone.png" alt="Previsualización en Ipad e Iphone">
-						</div>
-						<div class="col-md-6 descripcio">
-							<h3><?php echo $text['Etapa. Titol 2']; ?></h3>
-							<p><?php echo $text['Etapa. Descripcio 2']; ?></p>
-						</div>
-						<div class="col-md-6 no-mobile">
-							<img src="img/serveis/ipad+iphone.png" alt="Previsualización en Ipad e Iphone">
-						</div>
-					</div>
-				</div>
-			</section>
-
-			<section class="bg-third" id="pricing">
-				<div class='pricing pricing-palden'>
-
-					<div class='pricing-item'>
-					  <div class='pricing-deco'>
-					    <svg class='pricing-deco-img' enable-background='new 0 0 300 100' height='100px' id='Layer_1' preserveAspectRatio='none' version='1.1' viewBox='0 0 300 100' width='300px' x='0px' xml:space='preserve' xmlns:xlink='http://www.w3.org/1999/xlink' xmlns='http://www.w3.org/2000/svg' y='0px'>
-					      <path class='deco-layer deco-layer--1' d='M30.913,43.944c0,0,42.911-34.464,87.51-14.191c77.31,35.14,113.304-1.952,146.638-4.729&#x000A;	c48.654-4.056,69.94,16.218,69.94,16.218v54.396H30.913V43.944z' fill='#FFFFFF' opacity='0.6'></path>
-					      <path class='deco-layer deco-layer--2' d='M-35.667,44.628c0,0,42.91-34.463,87.51-14.191c77.31,35.141,113.304-1.952,146.639-4.729&#x000A;	c48.653-4.055,69.939,16.218,69.939,16.218v54.396H-35.667V44.628z' fill='#FFFFFF' opacity='0.6'></path>
-					      <path class='deco-layer deco-layer--3' d='M43.415,98.342c0,0,48.283-68.927,109.133-68.927c65.886,0,97.983,67.914,97.983,67.914v3.716&#x000A;	H42.401L43.415,98.342z' fill='#FFFFFF' opacity='0.7'></path>
-					      <path class='deco-layer deco-layer--4' d='M-34.667,62.998c0,0,56-45.667,120.316-27.839C167.484,57.842,197,41.332,232.286,30.428&#x000A;	c53.07-16.399,104.047,36.903,104.047,36.903l1.333,36.667l-372-2.954L-34.667,62.998z' fill='#FFFFFF'></path>
-					    </svg>
-					    <div class='pricing-price'><span class='pricing-currency'>€</span>0.00
-					      <span class='pricing-period'>/ month</span>
-					    </div>
-					    <h3 class='pricing-title'>Constructor / Assurance</h3>
-					  </div>
-					  <ul class='pricing-feature-list'>
-					    <li class='pricing-feature'>Unlimited buildings data</li>
-					    <li class='pricing-feature'>Installation at 20.00€/element</li>
-					    <li class='pricing-feature'>Standard control rules</li>
-					  </ul>
-					  <a href="//app.mesural.com/register.php?profile=constructor" class='pricing-action'>Choose plan</a>
-					</div>
-
-					<div class='pricing-item pricing__item--featured'>
-					  <div class='pricing-deco'>
-					    <svg class='pricing-deco-img' enable-background='new 0 0 300 100' height='100px' id='Layer_1' preserveAspectRatio='none' version='1.1' viewBox='0 0 300 100' width='300px' x='0px' xml:space='preserve' xmlns:xlink='http://www.w3.org/1999/xlink' xmlns='http://www.w3.org/2000/svg' y='0px'>
-					      <path class='deco-layer deco-layer--1' d='M30.913,43.944c0,0,42.911-34.464,87.51-14.191c77.31,35.14,113.304-1.952,146.638-4.729&#x000A;	c48.654-4.056,69.94,16.218,69.94,16.218v54.396H30.913V43.944z' fill='#FFFFFF' opacity='0.6'></path>
-					      <path class='deco-layer deco-layer--2' d='M-35.667,44.628c0,0,42.91-34.463,87.51-14.191c77.31,35.141,113.304-1.952,146.639-4.729&#x000A;	c48.653-4.055,69.939,16.218,69.939,16.218v54.396H-35.667V44.628z' fill='#FFFFFF' opacity='0.6'></path>
-					      <path class='deco-layer deco-layer--3' d='M43.415,98.342c0,0,48.283-68.927,109.133-68.927c65.886,0,97.983,67.914,97.983,67.914v3.716&#x000A;	H42.401L43.415,98.342z' fill='#FFFFFF' opacity='0.7'></path>
-					      <path class='deco-layer deco-layer--4' d='M-34.667,62.998c0,0,56-45.667,120.316-27.839C167.484,57.842,197,41.332,232.286,30.428&#x000A;	c53.07-16.399,104.047,36.903,104.047,36.903l1.333,36.667l-372-2.954L-34.667,62.998z' fill='#FFFFFF'></path>
-					    </svg>
-					    <div class='pricing-price'><span class='pricing-currency'>€</span>2.99
-					      <span class='pricing-period'>/ month</span>
-					    </div>
-					    <h3 class='pricing-title'>Architect</h3>
-					  </div>
-					  <ul class='pricing-feature-list'>
-					    <li class='pricing-feature'>2 buildings data</li>
-					    <li class='pricing-feature'>Installation at 0.00€/element</li>
-					    <li class='pricing-feature'>Standard control rules</li>
-					  </ul>
-					  <a href="//app.mesural.com/register.php?profile=architect" class='pricing-action'>Choose plan</a>
-					</div>
-
-					<div class='pricing-item'>
-					  <div class='pricing-deco'>
-					    <svg class='pricing-deco-img' enable-background='new 0 0 300 100' height='100px' id='Layer_1' preserveAspectRatio='none' version='1.1' viewBox='0 0 300 100' width='300px' x='0px' xml:space='preserve' xmlns:xlink='http://www.w3.org/1999/xlink' xmlns='http://www.w3.org/2000/svg' y='0px'>
-					      <path class='deco-layer deco-layer--1' d='M30.913,43.944c0,0,42.911-34.464,87.51-14.191c77.31,35.14,113.304-1.952,146.638-4.729&#x000A;	c48.654-4.056,69.94,16.218,69.94,16.218v54.396H30.913V43.944z' fill='#FFFFFF' opacity='0.6'></path>
-					      <path class='deco-layer deco-layer--2' d='M-35.667,44.628c0,0,42.91-34.463,87.51-14.191c77.31,35.141,113.304-1.952,146.639-4.729&#x000A;	c48.653-4.055,69.939,16.218,69.939,16.218v54.396H-35.667V44.628z' fill='#FFFFFF' opacity='0.6'></path>
-					      <path class='deco-layer deco-layer--3' d='M43.415,98.342c0,0,48.283-68.927,109.133-68.927c65.886,0,97.983,67.914,97.983,67.914v3.716&#x000A;	H42.401L43.415,98.342z' fill='#FFFFFF' opacity='0.7'></path>
-					      <path class='deco-layer deco-layer--4' d='M-34.667,62.998c0,0,56-45.667,120.316-27.839C167.484,57.842,197,41.332,232.286,30.428&#x000A;	c53.07-16.399,104.047,36.903,104.047,36.903l1.333,36.667l-372-2.954L-34.667,62.998z' fill='#FFFFFF'></path>
-					    </svg>
-					    <div class='pricing-price'><span class='pricing-currency'>€</span>4.99
-					      <span class='pricing-period'>/ month</span>
-					    </div>
-					    <h3 class='pricing-title'>Engineer / OCT</h3>
-					  </div>
-					  <ul class='pricing-feature-list'>
-					    <li class='pricing-feature'>5 buildings data</li>
-					    <li class='pricing-feature'>Installation at 0.00€/element</li>
-					    <li class='pricing-feature'>Customizable control rules</li>
-					  </ul>
-					  <a href="//app.mesural.com/register.php?profile=engineer" class='pricing-action'>Choose plan</a>
-					</div>
-
+					<h2>Now and remotely</h2>
+					<h3>On construction, at the office, at home: in the same way and on real time</h3>
+					<img src="img/devices.png" />
+					<p>Imagina, crea, modifica en tiempo real. Nunca ha sido tan fácil tomar decisiones del mismo modo en la obra que desde el sofá de tu casa.<br><br>Tú decides cómo te ayudamos. Deja que la Cápsula de Mesural haga el resto.</p>
+					<a href="#">More information</a>
 				</div>
 			</section>
 
