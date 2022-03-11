cb9c9fd6f06f1aae9f763af4ac18f3a005c5fef3
roigagusti <gutiroma@hotmail.com>
2022-03-11 17:41:47 +0100

    suprimint elements antics

--- app_old/sections/metadata.php
+++
@@ -1,6 +0,0 @@
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-<meta name="description" content="Mesural es la tecnología de fácil instalación que monitoriza estructuras a tiempo real y de forma remota garantizando la seguridad en la arquitectura gracias a la tranquilidad que ofrecen los datos.">
-<meta property="image" content="img/mini-marca.jpg">
-<meta name="author" content="Aldasoro">
